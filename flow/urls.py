fbc98674209a67daed51bf78d65677dff3e507fd
amaozhao <amaozhao@gmail.com>
2024-10-24 15:30:54 +0800

    update

--- flow/urls.py
+++ flow/urls.py
@@ -25,5 +25,5 @@ urlpatterns = [
     path("wechat/callback/", WechatCallback.as_view(), name="wechat_callback"),
     path("google/login_url/", GoogleLoginUrl.as_view(), name='google_login_url'),
     path("google/login/", GoogleLoginView.as_view(), name="google_login"),
-    path("google/callback/", GoogleCallback.as_view(), name="google_callback"),
+    path("google/callback/", GoogleCallback.as_view(), name="flow_google_callback"),
 ]
