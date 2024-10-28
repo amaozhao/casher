55b9d43b1a7aed671571782a9543976f5d860499
amaozhao <amaozhao@gmail.com>
2024-10-28 13:12:59 +0800

    update

--- accounts/urls.py
+++ accounts/urls.py
@@ -9,5 +9,5 @@ from accounts.views import (
 urlpatterns = [
     path("google/login_url/", GoogleLoginUrl.as_view(), name='google_login_url'),
     path("google/login/", GoogleLoginView.as_view(), name="google_login"),
-    path("google/callback/", GoogleCallback.as_view(), name="flow_google_callback"),
+    path("google/callback/", GoogleCallback.as_view(), name="google_callback"),
 ]
