4e38ca389a6a94554d6c42bbf81c85954eb29ebc
amaozhao <amaozhao@gmail.com>
2024-10-28 13:00:42 +0800

    update

---
+++ accounts/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from accounts.views import (
+    GoogleLoginView,
+    GoogleCallback,
+    GoogleLoginUrl,
+)
+
+urlpatterns = [
+    path("google/login_url/", GoogleLoginUrl.as_view(), name='google_login_url'),
+    path("google/login/", GoogleLoginView.as_view(), name="google_login"),
+    path("google/callback/", GoogleCallback.as_view(), name="flow_google_callback"),
+]
