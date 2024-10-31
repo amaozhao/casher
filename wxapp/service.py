38babeae1b5dcccb4de28d4ca405ea444208563b
amaozhao <amaozhao@gmail.com>
2024-10-31 17:41:46 +0800

    update

--- wxapp/service.py
+++ wxapp/service.py
@@ -5,7 +5,7 @@ from django.conf import settings
 # 调用微信code2Session接口,换取用户唯一标识 OpenID 和 会话密钥 session_key
 def login(code):
     login_url = (
-        f"https://api.weixin.qq.com/sns/jscode2session?appid={settings.WXAPP_APPID}"
+        f"https://api.weixin.qq.com/sns/jscode2session?appid={settings.WEIXIN_APPID}"
         f"&secret={settings.WEIXIN_APPSECRET}&js_code={code}&grant_type=authorization_code"
     )
     response = requests.get(login_url)
