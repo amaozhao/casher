6829316c6d23ae78217e26bfab2563a5045b44a3
amaozhao <amaozhao@gmail.com>
2024-11-08 15:28:43 +0800

    update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,7 @@
+#!/bin/bash
+
+# 查找并杀掉所有运行中的 gunicorn 进程
+pkill -f 'gunicorn casher.asgi:application'
+
+# 启动新的 gunicorn 进程
 gunicorn casher.asgi:application -k uvicorn_worker.UvicornWorker -w 4 -b 0.0.0.0:8000 -D
