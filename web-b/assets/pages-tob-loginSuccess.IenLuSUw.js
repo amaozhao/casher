3c125e3940b94b4f8414377e67e01e43d22cffbf
amaozhao <amaozhao@gmail.com>
2024-11-11 19:37:26 +0800

    update web-b

---
+++ web-b/assets/pages-tob-loginSuccess.IenLuSUw.js
@@ -0,0 +1,1 @@
+import{f as a,w as e,i as s,o as t,k as o}from"./index-DZZGyAJD.js";import{_ as r}from"./_plugin-vue_export-helper.BCo6x5W8.js";const n=r({data:()=>({}),onShow:()=>{},methods:{}},[["render",function(r,n,d,c,i,p){const f=s;return t(),a(f,{class:"title"},{default:e((()=>[o("登录成功")])),_:1})}],["__scopeId","data-v-7626aeac"]]);export{n as default};
