f2abfa4dffb4dc01406ca0eaaffa188c7b758251
amaozhao <amaozhao@gmail.com>
2024-11-12 19:40:07 +0800

    fix bug

--- payment/serializers.py
+++ payment/serializers.py
@@ -16,17 +16,46 @@ class PagsmilePayoutSerializer(ModelSerializer):
 
 
 class WechatPayoutSerializer(ModelSerializer):
+    workflow_title = serializers.SerializerMethodField()
+    status = serializers.SerializerMethodField()
+    currency = serializers.SerializerMethodField()
+
     class Meta:
         model = WechatPayout
         fields = ("id", "pay", "status", "updated")
 
+    def get_currency(self, instance):
+        return "$" if instance.currency == 'USD' else "¥"
+
+    def get_status(self, instance):
+        if instance.status == 'success':
+            return "已付款"
+        return ""
+
+    def get_workflow_title(self, instance):
+        return instance.workflow.title
+
 
 class UserPayinSerializer(ModelSerializer):
     pay = serializers.SerializerMethodField()
+    workflow_title = serializers.SerializerMethodField()
+    status = serializers.SerializerMethodField()
+    currency = serializers.SerializerMethodField()
 
     class Meta:
         model = UserPayin
-        fields = ("id", "pay", "currency", "status", "updated")
+        fields = ("workflow_title", "id", "pay", "currency", "status", "updated")
 
     def get_pay(self, instance):
         return instance.fee
+
+    def get_currency(self, instance):
+        return "$" if instance.currency == 'USD' else "¥"
+
+    def get_status(self, instance):
+        if instance.status == 'success':
+            return "已付款"
+        return ""
+
+    def get_workflow_title(self, instance):
+        return instance.workflow.title
