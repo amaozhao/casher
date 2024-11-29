6114ff25572a52bdaf1b9aec7f6e9afcd68a7331
amaozhao <amaozhao@gmail.com>
2024-11-29 13:11:36 +0800

    update stripe

--- payment/services/stripe.py
+++ payment/services/stripe.py
@@ -5,16 +5,6 @@ import stripe
 from payment.models import UserHashrate
 
 
-def update_user_hashrate(user, amount):
-    user_hashrate = UserHashrate.objects.filter(user=user).first()
-    if not user_hashrate:
-        user_hashrate = UserHashrate.objects.create(user=user, hashrate=0)
-    init_rate = user_hashrate.hashrate
-    init_rate += amount * 100
-    user_hashrate.hashrate = init_rate
-    user_hashrate.save()
-
-
 @djstripe_receiver("checkout.session.completed")
 def handle_charge_succeeded(sender, **kwargs):
     event: Event = kwargs.get("event")
@@ -43,19 +33,6 @@ def bind_payment_method(user, payment_method_id):
         invoice_settings={"default_payment_method": payment_method_id},
     )
     print(f"绑定支付方式成功: {payment_method_id}")
-
-
-@djstripe_receiver("invoice.payment_succeeded")
-def invoice_payment_succeeded(sender, **kwargs):
-    event: Event = kwargs.get("event")
-    invoice_data = event.data['object']  # 获取 invoice 对象
-    invoice_id = invoice_data['id']
-    invoice = Invoice.objects.get(stripe_id=invoice_id)
-
-    # 处理支付成功的逻辑
-    # 比如，更新支付状态、通知用户等
-    invoice.status = "paid"
-    invoice.save()
 
 
 @djstripe_receiver("invoice.payment_failed")
