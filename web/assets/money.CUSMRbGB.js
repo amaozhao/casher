a2f58c01f0e0341bf67e936217c63ed5f78cd13c
amaozhao <amaozhao@gmail.com>
2024-12-18 09:12:58 +0800

    update domain

---
+++ web/assets/money.CUSMRbGB.js
@@ -0,0 +1,1 @@
+const s="/static/home2.svg",t="/static/me.svg",a="/static/clip_border.svg",c="/static/relation.svg",i="/static/money.svg";export{s as _,t as a,a as b,c,i as d};
---
+++ web/assets/uni-app.es.BozkVN2D.js
@@ -0,0 +1,1 @@
+import"./index-CYB8ynpA.js";function r(r,t){return"string"==typeof r?t:r}export{r};
