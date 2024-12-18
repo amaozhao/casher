fe2c81df3ec3ec10179e770e89b10ee7755438a4
amaozhao <amaozhao@gmail.com>
2024-12-18 11:20:37 +0800

    update domain

--- web/assets/uni-app.es.BozkVN2D.js
+++
@@ -1,1 +0,0 @@
-import"./index-CYB8ynpA.js";function r(r,t){return"string"==typeof r?t:r}export{r};
---
+++ web/assets/uni-app.es.DwCM6LJT.js
@@ -0,0 +1,1 @@
+import"./index-Dzheq5F4.js";function r(r,t){return"string"==typeof r?t:r}export{r};
